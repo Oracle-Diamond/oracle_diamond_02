72ee60392970ea0b87e2fccfcb640f2c62e0764d
MuhamadZulhilmi <92838961+MuhamadZulhilmi@users.noreply.github.com>
2022-12-01 13:03:38 +0800

    reserve button

--- lib/facilities.dart
+++ lib/facilities.dart
@@ -22,24 +22,38 @@ class _FacilitiesState extends State<Facilities> {
     );
 
     return Scaffold(
-      body: Image.asset(
-        "assets/images/badminton.jpg",
-        height: 300.0,
-        width: size.width,
-        fit: BoxFit.cover,
-      ),
-      backgroundColor: Color.fromARGB(255, 212, 210, 210),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 206, 84, 84),
-        toolbarHeight: 90.0,
-        leading: const IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: 'Navigation menu',
-          onPressed: null,
+        body: Image.asset(
+          "assets/images/badminton.jpg",
+          height: 300.0,
+          width: size.width,
+          fit: BoxFit.cover,
         ),
-        title: const Text('Facilities'),
-      ),
-    );
+        backgroundColor: Color.fromARGB(255, 212, 210, 210),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 206, 84, 84),
+          toolbarHeight: 90.0,
+          leading: const IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'Navigation menu',
+            onPressed: null,
+          ),
+          title: const Text('Facilities'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
+          tooltip: 'Add', // used by assistive technologies
+          onPressed: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (_) => HomePage()));
+          },
+          backgroundColor: const Color.fromARGB(255, 206, 84, 84),
+          child: const Text("Reserve",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 13.0,
+              )),
+        ));
   }
 }

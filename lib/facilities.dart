342ac2781c0f00dee4bfcb6b3010e0de011a57b6
MuhamadZulhilmi <92838961+MuhamadZulhilmi@users.noreply.github.com>
2022-12-01 12:42:51 +0800

    badminton page

--- lib/facilities.dart
+++ lib/facilities.dart
@@ -13,6 +13,7 @@ class Facilities extends StatefulWidget {
 class _FacilitiesState extends State<Facilities> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     Container(
       constraints: BoxConstraints.expand(),
       decoration: const BoxDecoration(
@@ -21,13 +22,17 @@ class _FacilitiesState extends State<Facilities> {
     );
 
     return Scaffold(
+      body: Image.asset(
+        "assets/images/badminton.jpg",
+        height: 300.0,
+        width: size.width,
+        fit: BoxFit.cover,
+      ),
       backgroundColor: Color.fromARGB(255, 212, 210, 210),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       appBar: AppBar(
         backgroundColor: Color.fromARGB(255, 206, 84, 84),
-        toolbarHeight: 200.0,
-        shape:
-            RoundedRectangleBorder(borderRadius: BorderRadius.circular(25.0)),
+        toolbarHeight: 90.0,
         leading: const IconButton(
           icon: Icon(Icons.menu),
           tooltip: 'Navigation menu',

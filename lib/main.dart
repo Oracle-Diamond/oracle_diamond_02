f1cc256d5e232151bfa8caa28a9bd3084727e23e
afiqzakwan <afiqzakwan.anuar@gmail.com>
2022-12-03 09:08:11 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,198 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,  //firebase line
   );                                                  //firebase line
   
+  runApp(const MyApp());
+
   //feature: booking calendar - function call
   initializeDateFormatting()
       .then((_) => runApp(const BookingCalendarDemoApp()));
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Future<FirebaseApp> _initializeFirebase() async {
+    FirebaseApp firebaseApp = await Firebase.initializeApp();
+    return firebaseApp;
+  }
+//Hi
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+        body: FutureBuilder(
+            future: _initializeFirebase(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return const LoginScreen();
+              }
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }));
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  //Login function
+  static Future<User?> loginUsingEmailPassword(
+      {required String email,
+      required String password,
+      required BuildContext context}) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user;
+    try {
+      UserCredential userCredential = await auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      user = userCredential.user;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == "user-not-found") {
+        print("No user found for that email");
+      }
+    }
+
+    return user;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //create the textfield controller
+    TextEditingController _emailController = TextEditingController();
+    TextEditingController _passwordController = TextEditingController();
+
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        //crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(
+            height: 5.0,
+          ),
+          const Text("User Login",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              )),
+          const SizedBox(
+            height: 25.0,
+          ),
+          Container(
+            height: 0.6,
+            width: 350.0,
+            color: Colors.grey,
+          ),
+          const SizedBox(
+            height: 25.0,
+          ),
+          Image.network(
+            // <-- SEE HERE
+            'https://upload.wikimedia.org/wikipedia/commons/c/cb/UTM-LOGO-FULL.png',
+            height: 150,
+            width: 290,
+            alignment: const Alignment(0, 0.5),
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          //const Text("Sports Hall Booking System",
+          // textAlign: TextAlign.center,
+          //style: TextStyle(
+          // color: Colors.black,
+          // fontSize: 30.0,
+          // fontWeight: FontWeight.normal)),
+          const SizedBox(
+            height: 30.00,
+          ),
+          TextField(
+            controller: _emailController,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(
+              hintText: "UTM Username",
+              prefixIcon: Icon(Icons.mail, color: Colors.black),
+              enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(12.0)))
+            ),
+          ),
+          const SizedBox(
+            height: 26.0,
+          ),
+          TextField(
+            controller: _passwordController,
+            obscureText: true,
+            decoration: const InputDecoration(
+              hintText: "Password",
+              prefixIcon: Icon(Icons.lock, color: Colors.black),
+              enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(12.0)))
+            ),
+          ),
+          const SizedBox(
+            height: 12.0,
+          ),
+          const Text("Forgot password?",
+              style: TextStyle(
+                color: Color.fromARGB(255, 206, 84, 84),
+              )),
+          const SizedBox(
+            height: 30.0,
+          ),
+          Container(
+            width: double.infinity,
+            child: RawMaterialButton(
+              fillColor: const Color.fromARGB(255, 206, 84, 84),
+              elevation: 0.0,
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12.0)),
+              onPressed: () async {
+                User? user = await loginUsingEmailPassword(
+                    email: _emailController.text,
+                    password: _passwordController.text,
+                    context: context);
+                print(user);
+                if (user != null) {
+                  Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) => ProfileScreen()));
+                }
+              },
+              child: const Text("Login",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                  )),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
 
 //feature: booking calendar - start
 class BookingCalendarDemoApp extends StatefulWidget {
@@ -57,9 +245,6 @@ class _BookingCalendarDemoAppState extends State<BookingCalendarDemoApp> {
   List<DateTimeRange> converted = [];
 
   List<DateTimeRange> convertStreamResultMock({required dynamic streamResult}) {
-    ///here you can parse the streamresult and convert to [List<DateTimeRange>]
-    ///take care this is only mock, so if you add today as disabledDays it will still be visible on the first load
-    ///disabledDays will properly work with real data
     DateTime first = now;
     DateTime second = now.add(const Duration(minutes: 55));
     DateTime third = now.subtract(const Duration(minutes: 240));

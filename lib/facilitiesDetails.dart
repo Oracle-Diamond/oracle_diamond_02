001ad1a081677a6c25688d2a36cae87347346075
MuhamadZulhilmi <92838961+MuhamadZulhilmi@users.noreply.github.com>
2022-12-04 02:29:58 +0800

    list facilities

--- lib/facilitiesDetails.dart
+++ lib/facilitiesDetails.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:oracle_diamond_02/main.dart';
 import 'package:oracle_diamond_02/assets.dart';
 import 'package:oracle_diamond_02/DetailsDataModel.dart';
 
@@ -25,120 +24,47 @@ class _FacilitiesState extends State<FacilitiesDetails> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 209, 195, 195),
       appBar: AppBar(
         title: Text(widget.detailsDataModel[widget.index].name),
-        backgroundColor: const Color.fromARGB(255, 206, 84, 84),
-        toolbarHeight: 50.0,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
-        elevation: 0,
-        leading: Icon(Icons.menu),
-        actions: <Widget>[
-          Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Container(
-              width: 36,
-              height: 30,
-              decoration: BoxDecoration(
-                  color: Colors.grey[800],
-                  borderRadius: BorderRadius.circular(10)),
-              child: Center(child: Text("0")),
-            ),
-          )
-        ],
       ),
-      body: SafeArea(
-        child: Container(
-          padding: EdgeInsets.all(20.0),
-          child: Column(
-            children: <Widget>[
-              Container(
-                width: double.infinity,
-                height: 200,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    image: DecorationImage(
-                        image: NetworkImage(
-                            'https://upload.wikimedia.org/wikipedia/commons/c/cb/UTM-LOGO-FULL.png'),
-                        fit: BoxFit.cover)),
-                child: Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      gradient:
-                          LinearGradient(begin: Alignment.bottomRight, colors: [
-                        Colors.black.withOpacity(.4),
-                        Colors.black.withOpacity(.2),
-                      ])),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      Text(
-                        "Facilities",
-                        style: TextStyle(
-                            color: Color.fromARGB(255, 212, 210, 210),
-                            fontSize: 35,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(
-                        height: 30,
-                      ),
-                      SizedBox(
-                        height: 30,
-                      ),
-                    ],
-                  ),
-                ),
+      body: Column(
+        children: [
+          Image.network(widget.detailsDataModel[widget.index].ImageUrl),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            widget.detailsDataModel[widget.index].desc,
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              FloatingActionButton(
+                heroTag: "f1",
+                onPressed: () {
+                  setState(() {
+                    if (widget.index != 0) {
+                      widget.index--;
+                    }
+                  });
+                },
+                child: Icon(Icons.arrow_back_ios),
               ),
-              SizedBox(
-                height: 20,
+              FloatingActionButton(
+                heroTag: "f2",
+                onPressed: () {
+                  setState(() {
+                    if (widget.index != widget.detailsDataModel.length - 1) {
+                      widget.index++;
+                    }
+                  });
+                },
+                child: Icon(Icons.arrow_forward_ios),
               ),
-              Expanded(
-                  child: GridView.count(
-                crossAxisCount: 2,
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
-                children: _listItem
-                    .map((item) => Card(
-                          color: Colors.transparent,
-                          elevation: 0,
-                          child: Container(
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20),
-                                image: DecorationImage(
-                                    image: NetworkImage(item),
-                                    fit: BoxFit.cover)),
-                            child: Transform.translate(
-                              offset: Offset(50, -50),
-                              child: Container(
-                                margin: EdgeInsets.symmetric(
-                                    horizontal: 65, vertical: 63),
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(10),
-                                    color: Colors.white),
-                                child: Icon(
-                                  Icons.bookmark_border,
-                                  size: 15,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ))
-                    .toList(),
-              ))
             ],
           ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        heroTag: "f2",
-        onPressed: () {
-          setState(() {
-            if (widget.index != widget.detailsDataModel.length - 1) {
-              widget.index++;
-            }
-          });
-        },
-        child: Icon(Icons.arrow_forward_ios),
+        ],
       ),
     );
   }
--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -13,20 +13,28 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   static List<String> facilitiesname = [
     'Badminton',
-    'Pingpong',
     'Tennis',
+    'Pingpong',
     'Futsal',
+  ];
+
+  static List<String> url = [
+    'https://firebasestorage.googleapis.com/v0/b/oracle-diamond-02.appspot.com/o/badminton.jpg?alt=media',
+    'https://firebasestorage.googleapis.com/v0/b/oracle-diamond-02.appspot.com/o/tennis.jpg?alt=media',
+    'https://firebasestorage.googleapis.com/v0/b/oracle-diamond-02.appspot.com/o/pinpong.jpg?alt=media',
+    'https://firebasestorage.googleapis.com/v0/b/oracle-diamond-02.appspot.com/o/futsal.jpg?alt=media'
   ];
 
   final List<FacilitiesDataModel> facilities = List.generate(
       facilitiesname.length,
-      (index) => FacilitiesDataModel('${facilitiesname[index]}', '${[index]}',
-          '${facilitiesname[index]} Description...'));
+      (index) => FacilitiesDataModel('${facilitiesname[index]}',
+          '${url[index]}', '${facilitiesname[index]} Description...'));
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 206, 84, 84),
           title: Text('Profile Screen'),
         ),
         body: ListView.builder(

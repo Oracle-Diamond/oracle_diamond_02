66b25bd4656d7562d52b068571aca6f5ac2de68d
MuhamadZulhilmi <92838961+MuhamadZulhilmi@users.noreply.github.com>
2022-12-14 10:23:44 +0800

    add profile screen

--- lib/user_select.dart
+++ lib/user_select.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:oracle_diamond_02/profile_screen2.dart';
 import 'package:oracle_diamond_02/screen/facilities_list_screen.dart';
 
 class UserSelect extends StatefulWidget {
@@ -76,7 +77,7 @@ class _UserSelectState extends State<UserSelect> {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (context) => FacilitiesListScreen(),
+                      builder: (context) => ProfileScreen(),
                     ),
                   );
                 },

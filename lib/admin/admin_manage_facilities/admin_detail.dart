9c1f20b4c144093e42f6f8443796917cb03987a1
MuhamadZulhilmi <92838961+MuhamadZulhilmi@users.noreply.github.com>
2022-12-23 22:59:15 +0800

    Update admin_detail.dart

--- lib/admin/admin_manage_facilities/admin_detail.dart
+++ lib/admin/admin_manage_facilities/admin_detail.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:oracle_diamond_02/main.dart';
+import 'package:oracle_diamond_02/admin/profile_screen2.dart';
 import 'package:oracle_diamond_02/user/facilities/screen/model/facilities.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class FacilitiesAdminDetails extends StatelessWidget {
   final Facilities facilities;
@@ -9,98 +10,124 @@ class FacilitiesAdminDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          Container(
-              foregroundDecoration: BoxDecoration(color: Colors.black26),
-              height: 400,
-              child: Image.network(facilities.ImageUrl, fit: BoxFit.cover)),
-          SingleChildScrollView(
-            padding: const EdgeInsets.only(top: 16.0, bottom: 20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                const SizedBox(height: 250),
-                Row(
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                      child: Text(
-                        facilities.name,
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 28.0,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    const SizedBox(width: 16.0),
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 8.0,
-                        horizontal: 16.0,
-                      ),
-                    ),
-                  ],
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        elevation: 1,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+          onPressed: () {
+            Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => HomePageWidget()));
+          },
+        ),
+      ),
+      body: Container(
+          padding: const EdgeInsets.only(left: 16, top: 10, right: 16),
+          child: GestureDetector(
+              onTap: () {
+                FocusScope.of(context).unfocus();
+              },
+              child: ListView(children: [
+                const Text(
+                  "Mon, 15/11/2022",
+                  style: TextStyle(fontSize: 15),
                 ),
-                Container(
-                  padding: const EdgeInsets.all(32.0),
-                  color: Colors.white,
-                  child: Column(
+                const Text(
+                  "Facilities Management",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+                ),
+                SizedBox(height: 20.0),
+                Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: <Widget>[
-                      const SizedBox(height: 30.0),
-                      SizedBox(
-                        width: double.infinity,
-                        height: 50,
-                        child: ElevatedButton(
-                          child: Text(
-                            "View Overall Time Table",
-                            style: TextStyle(fontWeight: FontWeight.normal),
-                          ),
-                          onPressed: () {
-                            Navigator.of(context).push(MaterialPageRoute(
-                                builder: (_) => BookingCalendarDemoApp()));
-                          },
-                          style: ButtonStyle(
-                            backgroundColor:
-                                MaterialStateProperty.all(Colors.red),
-                          ),
+                      Container(
+                        width: 150.0,
+                        height: 150.0,
+                        child: CircleAvatar(
+                          radius: 75.0,
+                          backgroundImage: NetworkImage(facilities.ImageUrl),
                         ),
                       ),
-                      const SizedBox(height: 30.0),
-                      Text(
-                        facilities.name.toUpperCase(),
-                        textAlign: TextAlign.left,
-                        style: TextStyle(
-                            fontWeight: FontWeight.w600, fontSize: 18.0),
-                      ),
-                      const SizedBox(height: 10.0),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Positioned(
-            top: 0,
-            left: 0,
-            right: 0,
-            child: AppBar(
-              backgroundColor: Colors.transparent,
-              elevation: 0,
-              centerTitle: true,
-              title: Text(
-                "Facilities Management",
-                style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.normal),
-              ),
-            ),
-          ),
-        ],
-      ),
+                    ]),
+                SizedBox(height: 20.0),
+                Stack(
+                  alignment: AlignmentDirectional.center,
+                  children: [
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              SizedBox(height: 20.0),
+                              Text(
+                                "Facility Name",
+                                style: TextStyle(fontSize: 18.0),
+                              ),
+                              SizedBox(width: 10.0),
+                              Icon(
+                                FontAwesomeIcons.pen,
+                                size: 12.0,
+                                color: Colors.black54,
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 10.0),
+                          Text(
+                            facilities.name,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 20.0),
+                          ),
+                          SizedBox(height: 10.0),
+                          Row(
+                            children: <Widget>[
+                              Text(
+                                "Facility Type",
+                                style: TextStyle(fontSize: 18.0),
+                              ),
+                              SizedBox(width: 10.0),
+                              Icon(
+                                FontAwesomeIcons.pen,
+                                size: 12.0,
+                                color: Colors.black54,
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 10.0),
+                          Text(
+                            "Indoor",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 20.0),
+                          ),
+                          SizedBox(height: 10.0),
+                          Row(
+                            children: <Widget>[
+                              Text(
+                                "No of Court",
+                                style: TextStyle(fontSize: 18.0),
+                              ),
+                              SizedBox(width: 10.0),
+                              Icon(
+                                FontAwesomeIcons.pen,
+                                size: 12.0,
+                                color: Colors.black54,
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 10.0),
+                          Text(
+                            "9",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 20.0),
+                          ),
+                        ])
+                  ],
+                )
+              ]))),
     );
   }
 }

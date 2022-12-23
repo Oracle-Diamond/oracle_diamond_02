414c6839e61523226cb457a24bff6b728afb490b
MuhamadZulhilmi <92838961+MuhamadZulhilmi@users.noreply.github.com>
2022-12-24 02:16:45 +0800

    change profile

--- lib/user/user_select.dart
+++ lib/user/user_select.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:oracle_diamond_02/admin/admin_login.dart';
 import 'package:oracle_diamond_02/main.dart';
 import 'package:oracle_diamond_02/admin/profile_screen2.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class UserSelect extends StatefulWidget {
   const UserSelect({super.key});
@@ -14,84 +16,66 @@ class _UserSelectState extends State<UserSelect> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const SizedBox(
-            height: 5.0,
+        backgroundColor: Color(0xfff0f0f0),
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/background.jpg"),
+                fit: BoxFit.cover),
           ),
-          const Text("User Select",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 22.0,
-                fontWeight: FontWeight.bold,
-              )),
-          const SizedBox(
-            height: 25.0,
-          ),
-          Container(
-            height: 0.6,
-            width: 350.0,
-            color: Colors.grey,
-          ),
-          const SizedBox(
-            height: 25.0,
-          ),
-          GestureDetector(
-            onTap: () {
-              Navigator.of(context)
-                  .push(MaterialPageRoute(builder: (context) => AdminPage()));
-            },
-            child: Container(
-              margin: EdgeInsets.all(16),
-              height: 225,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-                color: Colors.red[900],
-              ),
-              width: double.infinity,
-              child: Align(
-                  alignment: Alignment.center,
-                  child: Text(
-                    "Admin",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 22.0,
-                      fontWeight: FontWeight.bold,
+          child: ListView(
+            children: [
+              SizedBox(height: 400),
+              GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => HomePage()));
+                },
+                child: Container(
+                  margin: EdgeInsets.all(16),
+                  height: 75,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: Colors.black,
+                  ),
+                  width: double.infinity,
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      "User Login ",
+                      style: GoogleFonts.poppins(
+                          fontSize: 18,
+                          color: Colors.white,
+                          letterSpacing: 0.168,
+                          fontWeight: FontWeight.w500),
                     ),
-                  )),
-            ),
-          ),
-          GestureDetector(
-            onTap: () {
-              Navigator.of(context)
-                  .push(MaterialPageRoute(builder: (context) => HomePage()));
-            },
-            child: Container(
-              margin: EdgeInsets.all(16),
-              height: 225,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-                color: Colors.red[900],
+                  ),
+                ),
               ),
-              width: double.infinity,
-              child: Align(
-                  alignment: Alignment.center,
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                Text(
+                  "Administrator? ",
+                  style: GoogleFonts.poppins(fontSize: 16, color: Colors.white),
+                ),
+                InkWell(
                   child: Text(
-                    "Student/Staff",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 22.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  )),
-            ),
+                    "Login Here",
+                    style: GoogleFonts.poppins(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        decoration: TextDecoration.underline,
+                        color: Colors.white),
+                  ),
+                  onTap: () {
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => AdminPage()));
+                  },
+                )
+              ])
+            ],
           ),
-        ],
-      ),
-    ));
+        ));
   }
 }
